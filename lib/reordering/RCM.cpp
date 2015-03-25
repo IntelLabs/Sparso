a68870cf75523a52baa0d3cfe22edf64b07cb724
Jongsoo Park <jongsoo.park@intel.com>
2015-03-24 22:11:14 -0700

    parallel bfs

--- lib/reordering/RCM.cpp
+++ lib/reordering/RCM.cpp
@@ -74,37 +74,128 @@ static void prefixSumOfLevels(
 int bfs(const CSR *A, int source, int *levels) {
   int numLevels = 0;
   levels[source] = numLevels;
-  numLevels++;
 
-  int *q = new int[A->m];
-  int *nextQ = new int[A->m];
-  q[0] = source;
-  int qTail = 1;
-  int nextQTail;
+  int *q[2];
+  q[0] = new int[A->m*omp_get_max_threads()];
+  q[1] = new int[A->m*omp_get_max_threads()];
+  q[0][0] = source;
 
-  while (qTail) {
-    nextQTail = 0;
-    for (int i = 0; i < qTail; ++i) {
-      int u = q[i];
-      assert(levels[u] == numLevels - 1);
+  int *qTail = new int[omp_get_max_threads()];
+  qTail[0] = 1;
 
-      for (int j = A->rowPtr[u]; j < A->rowPtr[u + 1]; ++j) {
-        int v = A->colIdx[j];
-        if (levels[v] == INT_MAX) {
-          levels[v] = levels[u] + 1;
-          nextQ[nextQTail] = v;
-          ++nextQTail;
-        }
+  int *qTailPrefixSum = new int[omp_get_max_threads() + 1];
+  qTailPrefixSum[0] = 0;
+
+  int *rowPtrs = new int[omp_get_max_threads()*A->m];
+  rowPtrs[0] = 0;
+  rowPtrs[1] = A->rowPtr[source + 1] - A->rowPtr[source];
+
+  int *nnzPrefixSum = new int[omp_get_max_threads() + 1];
+  nnzPrefixSum[0] = 0;
+  nnzPrefixSum[1] = rowPtrs[1];
+
+#pragma omp parallel
+  {
+  int tid = omp_get_thread_num();
+  int nthreads = omp_get_num_threads();
+
+  if (tid > 0) {
+    qTail[tid] = 0;
+    nnzPrefixSum[tid + 1] = 0;
+  }
+
+  while (true) {
+#pragma omp barrier
+#pragma omp master
+    {
+      for (int t = 0; t < nthreads; ++t) {
+        qTailPrefixSum[t + 1] = qTailPrefixSum[t] + qTail[t];
+        nnzPrefixSum[t + 1] += nnzPrefixSum[t];
       }
-    } // for each u in current level
+      ++numLevels;
+    }
+#pragma omp barrier
 
-    swap(q, nextQ);
-    qTail = nextQTail;
-    ++numLevels;
-  } // while q not empty
+    if (qTailPrefixSum[nthreads] == 0) break;
 
-  delete[] q;
-  delete[] nextQ;
+    // partition based on # of nnz
+    int nnzPerThread = (nnzPrefixSum[nthreads] + nthreads - 1)/nthreads;
+    int tBegin = upper_bound(
+        nnzPrefixSum, nnzPrefixSum + nthreads + 1,
+        nnzPerThread*tid) -
+      nnzPrefixSum - 1;
+    int tEnd = upper_bound(
+        nnzPrefixSum, nnzPrefixSum + nthreads + 1,
+        nnzPerThread*(tid + 1)) -
+      nnzPrefixSum - 1;
+    assert(tBegin >= 0 && tBegin <= nthreads);
+    assert(tEnd >= 0 && tEnd <= nthreads);
+    assert(tBegin <= tEnd);
+
+    int iBegin, iEnd;
+    if (0 == tid) {
+      iBegin = 0;
+    }
+    else if (tBegin == nthreads) {
+      iBegin = qTail[tEnd - 1];
+    }
+    else {
+      iBegin = upper_bound(
+          rowPtrs + tBegin*A->m, rowPtrs + tBegin*A->m + qTail[tBegin],
+          nnzPerThread*tid - nnzPrefixSum[tBegin]) -
+        (rowPtrs + tBegin*A->m) - 1;
+    }
+
+    if (tEnd == nthreads) {
+      iEnd = 0;
+    }
+    else {
+      iEnd = upper_bound(
+          rowPtrs + tEnd*A->m, rowPtrs + tEnd*A->m + qTail[tEnd],
+          nnzPerThread*(tid + 1) - nnzPrefixSum[tEnd]) -
+        (rowPtrs + tEnd*A->m) - 1;
+    }
+
+#pragma omp barrier
+
+    int *tailPtr = q[numLevels%2] + tid*A->m;
+    int *rowPtr = rowPtrs + tid*A->m;
+    *rowPtr = 0;
+
+    for (int t = tBegin; t <= tEnd; ++t) {
+      for (int i = (t == tBegin ? iBegin : 0);
+          i < (t == tEnd ? iEnd : qTail[t]);
+          ++i) {
+        int u = q[1 - numLevels%2][t*A->m + i];
+        assert(levels[u] == numLevels - 1);
+
+        for (int j = A->rowPtr[u]; j < A->rowPtr[u + 1]; ++j) {
+          int v = A->colIdx[j];
+          if (__sync_bool_compare_and_swap(levels + v, INT_MAX, numLevels)) {
+            *tailPtr = v;
+            *(rowPtr + 1) =
+              *rowPtr + A->rowPtr[v + 1] - A->rowPtr[v];
+
+            ++tailPtr;
+            ++rowPtr;
+          }
+        }
+      } // for each current node u
+    }
+
+#pragma omp barrier
+
+    qTail[tid] = tailPtr - (q[numLevels%2] + tid*A->m);
+    nnzPrefixSum[tid + 1] = *rowPtr;
+  } // while true
+  } // omp parallel
+
+  delete[] q[0];
+  delete[] q[1];
+  delete[] rowPtrs;
+  delete[] qTail;
+  delete[] qTailPrefixSum;
+  delete[] nnzPrefixSum;
 
   return numLevels;
 }
@@ -123,6 +214,7 @@ void CSR::getRCMPermutationNew(int *perm, int *inversePerm, int source /*=-1*/) 
   }
 
   int numLevels = bfs(this, source, levels);
+  printf("numLevels = %d\n", numLevels);
 
   // 3. Reorder
   int *prefixSum = new int[numLevels + 1];
