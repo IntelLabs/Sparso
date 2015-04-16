4c31a1dcc60407f90f87fa330af84894640996c1
Hongbo Rong <hongbo.rong@intel.com>
2015-04-15 21:22:36 -0700

    add manual pagerank_reordered for perf study

--- test/pagerank.jl
+++ test/pagerank.jl
@@ -34,6 +34,47 @@ function pagerank(A, p, r) # p: initial rank, r: damping factor
   toc()
 end
 
+# This is for performance study only
+function pagerank_reordered(A, p, r) # p: initial rank, r: damping factor
+  d = max(vec(sum(A, 2)), 1) # num of neighbors
+
+  time1 = time()
+  
+  __P_51227 = Array(Cint,size(A,2))
+  __Pprime_51228 = Array(Cint,size(A,2))
+  __A_51229 = SparseMatrixCSC(A.m,A.n,Array(Cint,size(A.colptr,1)),Array(Cint,size(A.rowval,1)),Array(Cdouble,size(A.nzval,1)))
+  CSR_ReorderMatrix(A,__A_51229,__P_51227,__Pprime_51228,true,true,true)
+  A = __A_51229
+
+  time2 = time()
+  
+  __p_51231 = Array(Cdouble,size(p,1))
+  reorderVector(p,__p_51231,__P_51227)
+  p = __p_51231
+  
+  time3 = time()
+  
+  __d_51231 = Array(Cdouble,size(p,1))
+  reorderVector(p,__d_51231,__P_51227)
+  d = __d_51231
+  
+  time4 = time()
+
+  for i = 1:100
+    q = p./d
+    Aq = A*q
+    p2 = r + (1-r)*Aq
+    p = p2
+  end
+  time5 = time()
+  println("Time of original loop= ", time5 - time1, " seconds") 
+  println("Breakdown:");
+  println("\tReorderMatrix A: ", time2 - time1, " seconds") 
+  println("\tReorderVector p: ", time3 - time2, " seconds") 
+  println("\tReorderVector d: ", time4 - time3, " seconds") 
+  println("\tLoop: ", time5 - time4, " seconds") 
+end
+
 A = MatrixMarket.mmread(ARGS[1])
 A = spones(A)
 
@@ -42,7 +83,7 @@ p = repmat([1/m], m)
 r = 0.15
 
 
-tests = 5
+tests = 1
 
 println("**** original pagerank perf")
 for i = 1 : tests
@@ -53,7 +94,8 @@ end
 println("**** accelerated pagerank perf")
 for i = 1 : tests
     p = repmat([1/m], m)
-    @acc pagerank(A, p, r)
+#    @acc pagerank(A, p, r)
+    pagerank_reordered(A, p, r)
 end
 
 
