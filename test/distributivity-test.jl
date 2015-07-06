166c32b3e58bd56ae35fe2429cb245adba1a94e9
Hongbo Rong <hongbo.rong@intel.com>
2015-07-06 16:29:22 -0700

    add test for distributivity analysis

---
+++ test/distributivity-test.jl
@@ -0,0 +1,27 @@
+using CompilerTools
+using CompilerTools.OptFramework
+
+include("../Src/SparseAccelerator.jl")
+using SparseAccelerator
+
+function checkDistributivity(ast, call_sig_arg_tuple, call_sig_args)
+  assert(typeof(ast)== Expr)
+  assert(ast.head == :lambda)
+
+  symbolInfo = SparseAccelerator.initSymbol2TypeDict(ast)
+  distributive = SparseAccelerator.checkDistributivity(ast, symbolInfo, true)
+  ast
+end
+
+sparse_pass = OptFramework.optPass(checkDistributivity, true)
+OptFramework.setOptPasses([sparse_pass])
+SparseAccelerator.set_debug_level(2)
+
+function call_SpMV(A, x)
+    return SparseAccelerator.SpMV(A, x)
+end
+
+m = 10
+A = sprand(m, m, 0.1)
+x = repmat([1/m], m)
+@acc call_SpMV(A, x)
