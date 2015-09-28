15bc52364ac5060820acd3af92832b6f164de8c0
Jongsoo Park <jongsoo.park@intel.com>
2015-09-27 21:56:55 -0700

    run accelerated pagerank twice; more detailed timing breakdown

--- test/correctness/pagerank.jl
+++ test/correctness/pagerank.jl
@@ -1,12 +1,25 @@
 include("../../src/SparseAccelerator.jl")
 using SparseAccelerator
 
-set_options(SA_ENABLE, SA_VERBOSE, SA_USE_SPMP, SA_REORDER)
+set_options(SA_ENABLE, SA_VERBOSE, SA_USE_SPMP, SA_CONTEXT, SA_REORDER)
+
+maxiter = 100
 
 function pagerank(A, p, r) # p: initial rank, r: damping factor
-  for i = 1:100
+  set_matrix_property(Dict(
+    :A => SA_SYMM_STRUCTURED | SA_SYMM_VALUED))
+
+  bytes = maxiter*(nnz(A)*12 + size(A, 1)*3*8)
+
+  t = time()
+
+  for i = 1:maxiter
     p = (1-r) *A * p + r
   end
+
+  t = time() - t
+  println("pagerank takes $t sec ($(bytes/t/1e9) gbps)")
+
   p
 end
 
@@ -25,5 +38,23 @@ x = pagerank(A, p, r)
 println("\tOriginal sum of x=", sum(x))
 
 println("\nAccelerated: ")
+SparseAccelerator.reset_spmp_spmv_time()
+SparseAccelerator.reset_knob_spmv_time()
 @acc x= pagerank(A, p, r)
+t = SparseAccelerator.get_spmp_spmv_time()
+bytes = maxiter*(nnz(A)*12 + m*3*8)
+println("time spent on spmp spmv $t sec ($(bytes/t/1e9) gbps)")
+t = SparseAccelerator.get_knob_spmv_time()
+println("time spent on knob spmv $t sec ($(bytes/t/1e9) gbps)")
+println("\tAccelerated sum of x=", sum(x))
+
+println("\nAccelerated2: ")
+SparseAccelerator.reset_spmp_spmv_time()
+SparseAccelerator.reset_knob_spmv_time()
+@acc x= pagerank(A, p, r)
+t = SparseAccelerator.get_spmp_spmv_time()
+bytes = maxiter*(nnz(A)*12 + m*3*8)
+println("time spent on spmp spmv $t sec ($(bytes/t/1e9) gbps)")
+t = SparseAccelerator.get_knob_spmv_time()
+println("time spent on knob spmv $t sec ($(bytes/t/1e9) gbps)")
 println("\tAccelerated sum of x=", sum(x))
