5f7b46e67ebdbf14f50bbe0c9c748602966ceb57
Lingxiang Xiang <lingxiang.xiang@intel.com>
2015-10-06 02:36:37 -0700

    rules for correct upper_of analysis code refactoring for analysis passes

--- src/property-lowerupper.jl
+++ src/property-lowerupper.jl
@@ -33,11 +33,11 @@ type LowerUpperProperty <: MatrixProperty
             pmap = call_sites.extra.local_map 
         else
             pmap = call_sites.extra.property_map
-            if value != 0 && (!haskey(pmap, sym_name) || pmap[sym_name] != value)
+            if value != nothing && (!haskey(pmap, sym_name) || pmap[sym_name] != value)
                 call_sites.extra.changed = true
-                dprintln(1, 1, "val changed: ", sym_name, value)
             end
         end
+        dprintln(1, 1, "update val: ", sym_name,"=", value)
         pmap[sym_name] = value
     end
 
@@ -66,6 +66,8 @@ type LowerUpperProperty <: MatrixProperty
         reordering_power  :: Int,
         reordering_FAR    :: Tuple
     )
+        dprintln(1, 1,"\nasdfasdfasdf")
+        dump(last(ast.args))
         A = get_symexpr(last(ast.args))
         set_property_val(call_sites, ast, get_property_val(call_sites, A))
         return true
@@ -103,7 +105,7 @@ type LowerUpperProperty <: MatrixProperty
         vRHS = get_property_val(call_sites, RHS) 
         vLHS = get_property_val(call_sites, LHS)
 
-        if vRHS !=0 && vLHS != vRHS
+        if vRHS !=nothing && vLHS != vRHS
             set_property_val(call_sites, LHS, vRHS)
         end
         return true
@@ -205,27 +207,9 @@ type LowerUpperProperty <: MatrixProperty
     )
         A = ast.args[2]
         dia = call_sites.extra.local_map[:SA_DIAGONAL] 
-        print("****************")
         #dump(A)
         #dump(dia)
         return in(A, dia)
-    end
-
-    @doc """ Pre-processing function. Check if arg2 is a sparse diagonal matrix """
-    function CS_apply_type_check(
-        ast           :: Expr,
-        call_sites    :: CallSites,
-        fknob_creator :: AbstractString,
-        fknob_deletor :: AbstractString
-    )
-        print("==========")
-        f = ast.args[2]
-
-        if f.head == :call 
-            m, func_name = resolve_call_names(f.args)
-            return func_name == "apply_type" 
-        end
-        return false
     end
 
     const CS_spdiagm_times_any_pattern = ExprPattern(
@@ -244,9 +228,9 @@ type LowerUpperProperty <: MatrixProperty
 
     const CS_apply_type_pattern = ExprPattern(
         "CS_apply_type_pattern",
-        (:call, Expr(:call, TopNode(:apply_type), Any, Any, Any), SparseMatrixCSC),
+        (:call, TypedExprNode(Function, :call, TopNode(:apply_type), GlobalRef(Main, :SparseMatrixCSC), GlobalRef(Main, :Cdouble), GlobalRef(Main, :Cint)), Any),
         (:NO_SUB_PATTERNS,),
-        CS_apply_type_check,
+        do_nothing,
         (:NO_CHANGE, ),
         propagate_last_symbol_property,
         "",
@@ -316,7 +300,21 @@ type LowerUpperProperty <: MatrixProperty
 
     const CS_assign_pattern = ExprPattern(
         "CS_assign_pattern",
-        (:(=), SparseMatrixCSC, SparseMatrixCSC),
+        (:(=), Any, SparseMatrixCSC),
+        (:NO_SUB_PATTERNS,),
+        do_nothing,
+        (:NO_CHANGE, ),
+        post_assign_action,
+        "",
+        "",
+        (),
+        0,
+        ()
+    )
+
+    const CS_assign2_pattern = ExprPattern(
+        "CS_assign2_pattern",
+        (:(=), SparseMatrixCSC, Any),
         (:NO_SUB_PATTERNS,),
         do_nothing,
         (:NO_CHANGE, ),
@@ -350,6 +348,7 @@ type LowerUpperProperty <: MatrixProperty
     """
     CS_lower_propagation_patterns = [
         CS_assign_pattern,
+        CS_assign2_pattern,
         CS_tril_pattern,
         CS_spdiagm_pattern,
         CS_spdiagm_times_any_pattern,
@@ -359,6 +358,7 @@ type LowerUpperProperty <: MatrixProperty
 
     CS_upper_propagation_patterns = [
         CS_assign_pattern,
+        CS_assign2_pattern,
         CS_triu_pattern,
         CS_spdiagm_pattern,
         CS_spdiagm_times_any_pattern,
@@ -440,8 +440,9 @@ type LowerUpperProperty <: MatrixProperty
         for (pname, property_map, CS_propagation_patterns) in 
             [("lower_of", property_lower_map, CS_lower_propagation_patterns),
              ("upper_of", property_upper_map, CS_upper_propagation_patterns)]
+
             dprintln(1, 1, "\nBefore " * pname * " anaylsis:")
-            #print_property_map(1, property_map, depend_map)
+            print_property_map(1, property_map, depend_map)
 
             # ctx_args is attached to call_sites so that
             # pattern functions can pass back their results
@@ -472,7 +473,7 @@ type LowerUpperProperty <: MatrixProperty
             end
 
             dprintln(1, 1, "\n", cnt, " iterations.\nAfter " * pname * " anaylsis:")
-            #print_property_map(1, property_map, depend_map)
+            print_property_map(1, property_map, depend_map)
 
             # copy back result
             delete!(property_map, :NEGATIVE_PROPERTY)
