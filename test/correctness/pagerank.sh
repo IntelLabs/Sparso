5011b5e0486e883dd5903d497770f0a41f78d889
Jongsoo Park <jongsoo.park@intel.com>
2015-10-07 10:56:50 -0700

    add pagerank.sh

--- test/correctness/pagerank.sh
+++ test/correctness/pagerank.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+. /opt/intel/compiler/latest/bin/compilervars.sh # make sure you also did this before compiling libcsr.so 
+# Use 1 socket, 1 HW thread per core
+export OMP_NUM_THREADS=14
+export KMP_AFFINITY=granularity=fine,compact,1
+
 for i in `cat pagerank.lst`; do
   echo !!!!$i
   echo %%%%auto-without-reordering
