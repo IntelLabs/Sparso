14d3c729c8751eed51338cd6d3bec81fc0283bfb
Lingxiang Xiang <lingxiang.xiang@intel.com>
2015-10-07 14:50:30 -0700

    disable precompilation for CompilerTools

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+echo "* Setting up submoduls"
 git submodule update --init
 
-echo "Setting up hooks"
+echo "* Setting up git hooks"
 cp ./scripts/post-checkout .git/hooks 
 cp ./scripts/post-rewrite .git/hooks 
 
-echo "Building SpMP lib"
-./scripts/update-SpMP.sh
-#/opt/intel/bin/compilervars.sh intel64
-cd ./lib
-make clean
-make -j DBG=yes
-cd ..
+if [ -f ~/.julia/lib/v0.4/CompilerTools.ji ]; then
+    echo "* Removing precompiled CompilerTools"
+    rm ~/.julia/lib/v0.4/CompilerTools.ji
+fi
 
-echo "Patching CompilerTools"
+echo "* Patching CompilerTools"
 cd deps/CompilerTools/
 patch -p 1 < ../../scripts/CompilerTools.patch
 cd - 
 
+echo "* Building SpMP lib"
+. ./scripts/update-SpMP.sh
+cd ./lib
+make clean
+make -j 
+cd ..
