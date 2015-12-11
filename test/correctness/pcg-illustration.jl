5741494b2813139258c214d97c69e565703d2ea0
Hongbo Rong <hongbo.rong@intel.com>
2015-12-11 09:53:26 -0800

    revised pcg-illustration.jl. closer to what paper says. generated code, but failed in lufact. to fix

--- test/correctness/pcg-illustration.jl
+++ test/correctness/pcg-illustration.jl
@@ -1,76 +1,47 @@
+# Note: tested with julia-b77587362d (0.5.0 dev) nightly build. Julia-0.4 has
+# issues with "\", and cannot run it through.
+ 
 include("../../src/SparseAccelerator.jl")
 include("./utils.jl")
 using SparseAccelerator
 
-function pcg_symgs(x, A, b, tol, maxiter)
-    total_time = time()
-  
-    trsv_time = 0.
-    spmv_time = 0.
-    blas1_time = 0.
+set_options(SA_ENABLE, SA_VERBOSE, SA_USE_SPMP, SA_CONTEXT, SA_REORDER, SA_REPLACE_CALLS)
 
+function pcg_symgs(x, A, b, tolerance, maxiter)
+    total_time = time()
+
+    # TODO: replace this ILU0 with ICHOL
     L = tril(A)
     U  = SparseMatrixCSC{Cdouble, Cint}(spdiagm(1./diag(A)))*triu(A)
-
-    spmv_time -= time()
     r = b - A * x
-    spmv_time += time()
-
-    blas1_time -= time()
     normr0 = norm(r)
-    z = copy(r)
-    blas1_time += time()
-
-    rel_err = 1
-
-    trsv_time -= time()
-    Base.SparseMatrix.fwdTriSolve!(L, z)
-    Base.SparseMatrix.bwdTriSolve!(U, z)
-    trsv_time += time()
-
-    blas1_time -= time()
+    z = L \ r    
+    z = U \ z    
     p = copy(z)
     rz = dot(r, z)
-    blas1_time += time()
 
+    rel_err = 1
     k = 1
-    while k <= maxiter
+    while true
         old_rz = rz
-
-        spmv_time -= time()
         Ap = A*p
-        spmv_time += time()
-
-        blas1_time -= time()
         alpha = old_rz / dot(p, Ap)
         x += alpha * p
         r -= alpha * Ap
         rel_err = norm(r)/normr0
-        blas1_time += time()
-
-        if rel_err < tol 
+        if rel_err < tolerance 
             break
         end
-
-        blas1_time -= time()
-        z = copy(r)
-        blas1_time += time()
-
-        trsv_time -= time()
-        L = L \ z
-        U = U \ z
-        trsv_time += time()
-
-        blas1_time -= time()
+        
+        z = L \ r    
+        z = U \ z
         rz = dot(r, z)
-        beta = rz/old_rz
-        p = z + beta * p
-        blas1_time += time()
-
+        p = z + (rz/old_rz) * p
         k += 1
     end
+
     total_time = time() - total_time
-    println("total = $(total_time)s trsv_time = $(trsv_time)s ($((12.*(nnz(L) + nnz(U)) + 2.*8*(size(L, 1) + size(L, 2)))*k/trsv_time/1e9) gbps) spmv_time = $(spmv_time)s ($((12.*nnz(A) + 8.*(size(A, 1) + size(A, 2)))*(k + 1)/spmv_time/1e9) gbps) blas1_time = $blas1_time")
+    println("total = $(total_time)s")
 
     return x, k, rel_err
 end
@@ -81,22 +52,23 @@ else
     # This is for performance testing's purpose
     A = matrix_market_read(ARGS[1], true, true)
 end
-m       = size(A, 1)
-x       = zeros(Float64, m)
-b       = ones(Float64, m)
-tol     = 1e-7
-maxiter = 20000
+m         = size(A, 1)
+x         = zeros(Float64, m)
+b         = ones(Float64, m)
+tolerance = 1e-7
+maxiter   = 20000
 
-x, k, rel_err = pcg_symgs(x, A, b, tol, maxiter)
+# Run each code version twice. Measure the time for the second time.
+x, k, rel_err = pcg_symgs(x, A, b, tolerance, maxiter)
 x = zeros(Float64, m)
-x, k, rel_err = pcg_symgs(x, A, b, tol, maxiter)
+x, k, rel_err = pcg_symgs(x, A, b, tolerance, maxiter)
 println("\tOriginal sum of x=", sum(x))
 println("\tOriginal k=", k)
 println("\tOriginal rel_err=", rel_err)
 
-@acc x, k, rel_err = pcg_symgs(x, A, b, tol, maxiter)
+@acc x, k, rel_err = pcg_symgs(x, A, b, tolerance, maxiter)
 x = zeros(Float64, m)
-@acc x, k, rel_err = pcg_symgs(x, A, b, tol, maxiter)
+@acc x, k, rel_err = pcg_symgs(x, A, b, tolerance, maxiter)
 println("\tOptimized version: sum of x=", sum(x))
 println("\tOptimized version:  k=", k)
 println("\tOptimized version: rel_err=", rel_err)
